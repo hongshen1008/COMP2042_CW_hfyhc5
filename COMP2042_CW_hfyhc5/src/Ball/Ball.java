89d29419852efa5086d68bf2c8fc6f3a040e2222
hongshen <hongshen1008@gmail.com>
2021-11-30 14:54:03 +0800

    All files added with comments

---
+++ COMP2042_CW_hfyhc5/src/Ball/Ball.java
@@ -0,0 +1,161 @@
+package Ball;
+
+import java.awt.*;
+import java.awt.geom.Point2D;
+import java.awt.geom.RectangularShape;
+
+/**
+ * Created by filippo on 04/09/16.
+ *
+ */
+abstract public class Ball {
+
+    private Shape ballFace; //the shape of ball
+
+    private Point2D center;
+
+    private Point2D up;     //put access modifier and getter/setter method, so that it is accessible by other classes
+    private Point2D down;   //put access modifier and getter/setter method
+    private Point2D left;   //put access modifier and getter/setter method
+    private Point2D right;  //put access modifier and getter/setter method
+
+    private Color border;
+    private Color inner;
+
+    private int speedX;
+    private int speedY;
+
+    public Ball(Point2D center,int radiusA,int radiusB,Color inner,Color border){   //constructor
+        this.center = center;
+
+        setUp(new Point2D.Double());
+        setDown(new Point2D.Double());
+        setLeft(new Point2D.Double());
+        setRight(new Point2D.Double());
+
+        getUp().setLocation(center.getX(),center.getY()-(radiusB / 2));  //upper part of the ball
+        getDown().setLocation(center.getX(),center.getY()+(radiusB / 2)); //lower part of the ball
+
+        getLeft().setLocation(center.getX()-(radiusA /2),center.getY()); //left part of the ball
+        getRight().setLocation(center.getX()+(radiusA /2),center.getY()); //right part of the ball
+
+
+        ballFace = makeBall(center,radiusA,radiusB);
+        this.border = border;
+        this.inner  = inner;
+        speedX = 0;
+        speedY = 0;
+    }
+
+    protected abstract Shape makeBall(Point2D center,int radiusA,int radiusB);
+
+    public void move(){
+        RectangularShape tmp = (RectangularShape) ballFace;
+        center.setLocation((center.getX() + speedX),(center.getY() + speedY));
+        double w = tmp.getWidth();
+        double h = tmp.getHeight();
+
+        tmp.setFrame((center.getX() -(w / 2)),(center.getY() - (h / 2)),w,h);
+        setPoints(w,h);
+
+
+        ballFace = tmp;
+    }
+
+    public void setSpeed(int x,int y){
+        speedX = x;
+        speedY = y;
+    }
+
+    public void setXSpeed(int s){
+        speedX = s;
+    }
+
+    public void setYSpeed(int s){
+        speedY = s;
+    }
+
+    public void reverseX(){
+        speedX *= -1;
+    }
+
+    public void reverseY(){
+        speedY *= -1;
+    }
+
+    public Color getBorderColor(){
+        return border;
+    }
+
+    public Color getInnerColor(){
+        return inner;
+    }
+
+    public Point2D getPosition(){
+        return center;
+    }
+
+    public Shape getBallFace(){
+        return ballFace;
+    }
+
+    public void moveTo(Point p){
+        center.setLocation(p);
+
+        RectangularShape tmp = (RectangularShape) ballFace;
+        double w = tmp.getWidth();
+        double h = tmp.getHeight();
+
+        tmp.setFrame((center.getX() -(w / 2)),(center.getY() - (h / 2)),w,h);
+        ballFace = tmp;
+    }
+
+    private void setPoints(double width,double height){
+        getUp().setLocation(center.getX(),center.getY()-(height / 2));
+        getDown().setLocation(center.getX(),center.getY()+(height / 2));
+
+        getLeft().setLocation(center.getX()-(width / 2),center.getY());
+        getRight().setLocation(center.getX()+(width / 2),center.getY());
+    }
+
+    public int getSpeedX(){
+        return speedX;
+    }
+
+    public int getSpeedY(){
+        return speedY;
+    }
+
+
+    public Point2D getUp() {
+        return up;
+    }
+
+    public void setUp(Point2D up) {
+        this.up = up;
+    }
+
+    public Point2D getDown() {
+        return down;
+    }
+
+    public void setDown(Point2D down) {
+        this.down = down;
+    }
+
+    public Point2D getLeft() {
+        return left;
+    }
+
+    public void setLeft(Point2D left) {
+        this.left = left;
+    }
+
+    public Point2D getRight() {
+        return right;
+    }
+
+    public void setRight(Point2D right) {
+        this.right = right;
+    }
+}
